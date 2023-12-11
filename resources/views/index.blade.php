e8ad3fb07fb1ead06721571a76476aa02f17f49e
FraBona <francescobonafede08@gmail.com>
2023-12-11 16:43:24 +0100

    add header footer

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section>
+  <div class="container">
+    <h1>pagina home</h1>
+  </div>
+</section>
+
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+  <div class="container">
+    <h1>Questo e il footer</h1>
+  </div>
+</footer>
